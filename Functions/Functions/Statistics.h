2a9ae36ac68afb2ee5e32019bc17a6e0149a5cda
Дмитрий Кощеев <barada.90@yandex.ru>
2022-12-08 19:56:49 +0300

    11

--- Functions/Functions/Statistics.h
+++ Functions/Functions/Statistics.h
@@ -4,3 +4,4 @@ int Sum(int arr[], const int n);
 double Avg(int arr[], const int n);
 int minValueIn(int arr[], const int n);
 int maxValueIn(int arr[], const int n);
+void Sort(int arr[], const int n);
